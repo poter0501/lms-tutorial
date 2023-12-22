bf7214ff04e61b8bc23e8aebd13445e62bf4d03e
poter0501 <poter0501@gmail.com>
2023-12-22 12:44:01 +0900

    feat: chapter title, description 정보 입력 form 생성

---
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -0,0 +1,45 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { courseId: string; chapterId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const { ...values } = await req.json();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const courseOwner = await db.course.findUnique({
+      where: {
+        id: params.courseId,
+        userId: userId,
+      },
+    });
+
+    if (!courseOwner) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const chapter = await db.chapter.update({
+      where: {
+        id: params.chapterId,
+        courseId: params.courseId,
+      },
+      data: {
+        ...values
+      },
+    });
+
+    // TOD : handle videoUrl
+
+    return NextResponse.json(chapter);
+  } catch (error) {
+    console.log("[COURSE_CHAPTER_ID]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
